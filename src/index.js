d900998853ce76a003583aa417716c1cedd52247
AdityaK5081 <aditya.kanikdaley@exxonmobil.com>
2024-02-26 15:46:46 +0530

    horix height div added

--- src/index.js
+++ src/index.js
@@ -2,10 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Header from './Components/Header/Header';
+import MySelf from './Components/MySelf/MySelf';
+import Skills from './Components/Skills/Skills';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header />
+    <div className='horiz-height'></div>
+    <MySelf />
+    <div className='horiz-height'></div>
+    <Skills />
+    <div className='horiz-height'></div>
   </React.StrictMode>
 );
+
+// content lists
+// - skills
+// - hobbies
+// - previous work, internships
+// - certifications
+// - office fun - pics with StrictMode, Fathima, Emit-DND
