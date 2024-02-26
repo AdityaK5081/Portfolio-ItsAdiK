0d035a4af8ab75b8cf399e1b031a09058deb27b9
AdityaK5081 <aditya.kanikdaley@exxonmobil.com>
2024-02-26 18:33:31 +0530

    added basic skills-icons pack

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import '../Skills/Skills.scss'
 
+import { SiSplunk, SiJavascript, SiFlutter, SiDart, SiPython } from "react-icons/si";
+import { FaReact, FaJava } from "react-icons/fa";
+import { DiAndroid } from "react-icons/di";
+
+
+
 const skills = [
-    { id: 1, name: 'Splunk Cloud', image: '' },
-    { id: 2, name: 'React.JS', image: '' },
-    { id: 3, name: 'JavaScript', image: '' },
-    { id: 4, name: 'Python', image: '' }
+    { id: 1, name: 'Splunk', image: <SiSplunk/> },
+    { id: 2, name: 'React.JS', image: <FaReact/> },
+    { id: 3, name: 'JavaScript', image: <SiJavascript/> },
+    { id: 4, name: 'Flutter', image: <SiFlutter/> },
+    { id: 5, name: 'Dart', image: <SiDart/> },
+    { id: 6, name: 'Python', image: <SiPython/> },
+    { id: 7, name: 'Android', image: <DiAndroid/> },
+    { id: 8, name: 'Java', image: <FaJava/> },
 ]
 
 const Skills = () => {
@@ -16,8 +26,7 @@ const Skills = () => {
                     return <div key={skill.id} className='skill-parent'>
                         <div className='skill-child'>
                             {skill.image}
-                            <br />
-                            {skill.name}
+                            <p>{skill.name}</p>
                         </div>
                     </div>
                 })
