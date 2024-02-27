c77618b1230e70dc85c8faeb303cedb6cb010c84
AdityaK5081 <aditya.kanikdaley@exxonmobil.com>
2024-02-27 19:01:29 +0530

    skills updated

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -8,29 +8,32 @@ import { DiAndroid } from "react-icons/di";
 
 
 const skills = [
-    { id: 1, name: 'Splunk', image: <SiSplunk/> },
-    { id: 2, name: 'React.JS', image: <FaReact/> },
-    { id: 3, name: 'JavaScript', image: <SiJavascript/> },
-    { id: 4, name: 'Flutter', image: <SiFlutter/> },
-    { id: 5, name: 'Dart', image: <SiDart/> },
-    { id: 6, name: 'Python', image: <SiPython/> },
-    { id: 7, name: 'Android', image: <DiAndroid/> },
-    { id: 8, name: 'Java', image: <FaJava/> },
+    { id: 1, name: 'Splunk', image: <SiSplunk /> },
+    { id: 2, name: 'React.JS', image: <FaReact /> },
+    { id: 3, name: 'JavaScript', image: <SiJavascript /> },
+    { id: 4, name: 'Flutter', image: <SiFlutter /> },
+    { id: 5, name: 'Dart', image: <SiDart /> },
+    { id: 6, name: 'Python', image: <SiPython /> },
+    { id: 7, name: 'Android', image: <DiAndroid /> },
+    { id: 8, name: 'Java', image: <FaJava /> },
 ]
 
 const Skills = () => {
     return (
-        <div className='skills-container'>
-            {
-                skills.map((skill) => {
-                    return <div key={skill.id} className='skill-parent'>
-                        <div className='skill-child'>
-                            {skill.image}
-                            <p>{skill.name}</p>
+        <div id='timeline-container'>
+            <div>~My Skills~</div>
+            <div id='skills-container'>
+                {
+                    skills.map((skill) => {
+                        return <div key={skill.id} id='skill-parent'>
+                            <div id='skill-child'>
+                                {skill.image}
+                                <p>{skill.name}</p>
+                            </div>
                         </div>
-                    </div>
-                })
-            }
+                    })
+                }
+            </div>
         </div>
     )
 }
